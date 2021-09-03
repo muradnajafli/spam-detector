6e962f467e73b5613e7c546e7ac31dbb5ec11bf8
Alexandr_Lenivenko <alexandr.x.lenivenko@johnlewis.co.uk>
2021-09-03 10:00:17 +0300

    [proof-reading]: deleted

--- src/main/kotlin/exception/SpamDetectedException.kt
+++
@@ -1,8 +0,0 @@
-package exception
-
-import kotlin.coroutines.cancellation.CancellationException
-
-/**
- * Subclass for cancellation exception to specify when it was canceled by reason of spam detection in the text
- */
-class SpamDetectedException : CancellationException()
